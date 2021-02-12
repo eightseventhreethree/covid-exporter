1465ab47982bcc40f883b81fbe944e43a9b80c08
Rush S <rush@r3-mpbm1.ru8733.local>
2021-02-12 14:32:09 -0500

    removed shell for makefile

--- cleanup_start.sh
+++
@@ -1,3 +0,0 @@
-sudo docker stop covid-prometheus-exporter && sudo docker rm covid-prometheus-exporter
-sudo docker build . -t covid-prometheus-exporter
-sudo docker run --detach --publish 8000:8000 --name covid-prometheus-exporter -it --restart always covid-prometheus-exporter:latest
