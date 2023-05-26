56e56b77b2477d2fe220019085524864624256a6
Rush S <github@ru8733.com>
2023-05-25 22:06:41 -0400

    revert collector.py

--- python/collector.py
+++ python/collector.py
@@ -2,35 +2,16 @@ import time
 import requests
 import sys
 import datetime
-from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 from prometheus_client.core import GaugeMetricFamily, REGISTRY, CounterMetricFamily
 from prometheus_client import start_http_server
 
-
 class CustomCollector(object):
-    def __init__(self):
-        self.base_metric_name = "covid_19_"
-        covid_states = requests.get('https://disease.sh/v3/covid-19/states')
-        self.json_covid_states = covid_states.json()
-        #keys = json_covid_states[0].keys()
-
-    def _get_metrics(self, metric_name_arg):
-        metric_name = self.base_metric_name + metric_name_arg
-        c = CounterMetricFamily(metric_name, 'Help text', labels=['state'])
-        for state in self.json_covid_states:
-          c.add_metric([state['state']], state[metric_name_arg])
-        yield c
-
     def collect(self):
-        """
-        metric_name = cases
-        """
-        metrics_to_collect = ["cases", "todayCases"]
-        with ThreadPoolExecutor() as executor:
-            results = executor.map(self._get_metrics, metrics_to_collect)
-        yield results
+        base_metric_name = "covid_19_"
+        covid_states = requests.get('https://disease.sh/v3/covid-19/states')
+        json_covid_states = covid_states.json()
+        keys = json_covid_states[0].keys()
 
-    def cases(self):
         # cases
         metric_name = base_metric_name + "cases"
         c = CounterMetricFamily(metric_name, 'Help text', labels=['state'])
@@ -38,7 +19,6 @@ class CustomCollector(object):
           c.add_metric([state['state']], state['cases'])
         yield c
 
-    def todays_cases(self):
         # todays_cases
         metric_name = base_metric_name + "todayCases"
         c = CounterMetricFamily(metric_name, 'Help text', labels=['state'])
@@ -46,7 +26,6 @@ class CustomCollector(object):
           c.add_metric([state['state']], state['todayCases'])
         yield c
 
-    def deaths(self):
         # deaths
         metric_name = base_metric_name + "deaths"
         c = CounterMetricFamily(metric_name, 'Help text', labels=['state'])
@@ -54,7 +33,6 @@ class CustomCollector(object):
           c.add_metric([state['state']], state['deaths'])
         yield c
 
-    def today_deaths(self):
         # todayDeaths
         metric_name = base_metric_name + "todayDeaths"
         c = CounterMetricFamily(metric_name, 'Help text', labels=['state'])
@@ -62,7 +40,6 @@ class CustomCollector(object):
           c.add_metric([state['state']], state['todayDeaths'])
         yield c
 
-    def active(self):
         # active
         metric_name = base_metric_name + "active"
         c = CounterMetricFamily(metric_name, 'Help text', labels=['state'])
@@ -70,7 +47,6 @@ class CustomCollector(object):
           c.add_metric([state['state']], state['active'])
         yield c
 
-    def test(self):
         # tests
         metric_name = base_metric_name + "tests"
         c = CounterMetricFamily(metric_name, 'Help text', labels=['state'])
@@ -78,7 +54,6 @@ class CustomCollector(object):
           c.add_metric([state['state']], state['tests'])
         yield c
 
-    def test_per_one_million(self):
         # testsPerOneMillion
         metric_name = base_metric_name + "testsPerOneMillion"
         c = CounterMetricFamily(metric_name, 'Help text', labels=['state'])
@@ -88,12 +63,7 @@ class CustomCollector(object):
 
 if __name__ == '__main__':
     start_http_server(8000)
-    custom_collector = CustomCollector()
-    metrics_to_collect = ["cases", "todayCases"]
-    REGISTRY.register(*custom_collector)
-    #for result in results:
-    #    #print(*result)
-    #    REGISTRY.register(*result)
+    REGISTRY.register(CustomCollector())
     print('Registered custom metrics', flush=True)
     while True:
       x = datetime.datetime.now()
